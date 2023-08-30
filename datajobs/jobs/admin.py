971c630e225b5e9d042e7d271af6c34570229233
Thiago Paim <thpaim@gmail.com>
2023-08-30 01:19:34 -0300

    Adiciona model Job

--- datajobs/jobs/admin.py
+++ datajobs/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from jobs.models import Job
 
-# Register your models here.
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("id", "url", "title", "apply_url", "updated")
--- datajobs/jobs/models.py
+++ datajobs/jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    url = models.URLField(blank=True)
+    html = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, blank=True)
+    apply_url = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+    company_info = models.TextField(blank=True)
+    benefits = models.TextField(blank=True)
