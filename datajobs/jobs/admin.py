fd762f9bf09f2028eccdefb8be05b9e19d8940ca
Thiago Paim <thpaim@gmail.com>
2023-09-08 21:28:36 -0300

    Adds admin filters

--- datajobs/jobs/admin.py
+++ datajobs/jobs/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from jobs.models import Job
 
 
+class BaseInputFieldFilter(admin.SimpleListFilter):
+    template = "jobs/admin_input_filter.html"
+
+    def lookups(self, request, model_admin):
+        return ((None, None),)
+
+
+class TitleFilter(BaseInputFieldFilter):
+    title = "title"
+    parameter_name = "title"
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value:
+            return queryset.filter(title__icontains=value)
+
+
+class CompanyFilter(BaseInputFieldFilter):
+    title = "company"
+    parameter_name = "company"
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value:
+            return queryset.filter(company__icontains=value)
+
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ("id", "jobkey", "title", "company", "updated")
+    list_filter = (TitleFilter, CompanyFilter, "created", "updated")
